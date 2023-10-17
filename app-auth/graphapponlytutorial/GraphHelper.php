8d27bfc2c10d653fa0fc3ebde0ab5b29f7c7c55f
Jason Johnston <jasonjoh@users.noreply.github.com>
2023-10-17 13:00:46 -0400

    Update app auth to latest RC

--- app-auth/graphapponlytutorial/GraphHelper.php
+++ app-auth/graphapponlytutorial/GraphHelper.php
@@ -3,12 +3,10 @@
 // Licensed under the MIT license.
 
 // <UseSnippet>
-
-use Microsoft\Graph\Core\Authentication\GraphPhpLeagueAuthenticationProvider;
+use Microsoft\Graph\Core\Authentication\GraphPhpLeagueAccessTokenProvider;
 use Microsoft\Graph\Generated\Models;
 use Microsoft\Graph\Generated\Users\UsersRequestBuilderGetQueryParameters;
 use Microsoft\Graph\Generated\Users\UsersRequestBuilderGetRequestConfiguration;
-use Microsoft\Graph\GraphRequestAdapter;
 use Microsoft\Graph\GraphServiceClient;
 use Microsoft\Kiota\Authentication\Oauth\ClientCredentialContext;
 // </UseSnippet>
@@ -18,7 +16,7 @@ class GraphHelper {
     private static string $clientId = '';
     private static string $clientSecret = '';
     private static string $tenantId = '';
-    private static GraphPhpLeagueAuthenticationProvider $authProvider;
+    private static ClientCredentialContext $tokenContext;
     private static GraphServiceClient $appClient;
 
     public static function initializeGraphForAppOnlyAuth(): void {
@@ -26,25 +24,21 @@ class GraphHelper {
         GraphHelper::$clientSecret = $_ENV['CLIENT_SECRET'];
         GraphHelper::$tenantId = $_ENV['TENANT_ID'];
 
-        $tokenContext = new ClientCredentialContext(
+        GraphHelper::$tokenContext = new ClientCredentialContext(
             GraphHelper::$tenantId,
             GraphHelper::$clientId,
             GraphHelper::$clientSecret);
 
-        GraphHelper::$authProvider = new GraphPhpLeagueAuthenticationProvider(
-            $tokenContext,
-            ['https://graph.microsoft.com/.default']);
-
-        $adapter = new GraphRequestAdapter(GraphHelper::$authProvider);
-
-        GraphHelper::$appClient = new GraphServiceClient($adapter);
+        GraphHelper::$appClient = new GraphServiceClient(
+            GraphHelper::$tokenContext, ['https://graph.microsoft.com/.default']);
     }
     // </AppOnlyAuthConfigSnippet>
 
     // <GetAppOnlyTokenSnippet>
     public static function getAppOnlyToken(): string {
-        return GraphHelper::$authProvider
-            ->getAccessTokenProvider()
+        // Create an access token provider to get the token
+        $tokenProvider = new GraphPhpLeagueAccessTokenProvider(GraphHelper::$tokenContext);
+        return $tokenProvider
             ->getAuthorizationTokenAsync('https://graph.microsoft.com')
             ->wait();
     }
